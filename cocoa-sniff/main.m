1ffd22d26eec846f06091f49bdedeb33de6b706e
Alexis Gallagher <alexis@alexisgallagher.com>
2012-12-31 12:47:11 +0000

    rename

--- cocoa-sniff/main.m
+++ cocoa-sniff/main.m
@@ -10,30 +10,30 @@
 #import "PSLog.h"
 #import "ALGUtilities.h"
 
-void listSupportedEncodings(void);
-void p(NSString * s);
-void pln(NSString * s);
+void PrintSupportedEncodings(void);
+void PrintNSString(NSString * s);
+void PrintLnNSString(NSString * s);
 
 
-void p(NSString * s)
+void PrintNSString(NSString * s)
 {
   printf("%s",[s UTF8String]);
 }
-void pln(NSString * s)
+void PrintLnNSString(NSString * s)
 {
   printf("%s\n",[s UTF8String]);
 }
 
 
-void listSupportedEncodings()
+void PrintSupportedEncodings()
 {
-  pln(@"IANA_CharSetName\t\tNSSttringEncoding\\ttvalue");
+  PrintLnNSString(@"IANA_CharSetName\t\tNSSttringEncoding\\ttvalue");
   NSDictionary * namesAndValues = [ALGUtilities dictionaryWithNSStringEncodingNamesAndValues];
   [namesAndValues enumerateKeysAndObjectsUsingBlock:^(id encodingName, id encodingValue, BOOL *stop) {
     NSStringEncoding encoding = [encodingValue unsignedIntegerValue];
     NSString * IANAname = [ALGUtilities nameOfEncoding:encoding];
     
-    pln([NSString stringWithFormat:@"%@\t\t%@\t\t%@",IANAname,encodingName,encodingValue]);
+    PrintLnNSString([NSString stringWithFormat:@"%@\t\t%@\t\t%@",IANAname,encodingName,encodingValue]);
   }];
   
 }
@@ -44,14 +44,14 @@ int main (int argc, const char * argv[])
   @autoreleasepool {
     
     if (argc != 2) {
-      pln(@"usage: cocoa-sniff [filename]");
-      pln(@"usage: cocoa-sniff --list");
+      PrintLnNSString(@"usage: cocoa-sniff [filename]");
+      PrintLnNSString(@"usage: cocoa-sniff --list");
       exit(1);
     }
     
     NSString * arg = [NSString stringWithCString:argv[1] encoding:NSASCIIStringEncoding];
     if ([arg isEqualToString:@"--list"]) {
-      listSupportedEncodings();
+      PrintSupportedEncodings();
     }
     else {
       // try utf-8, then windows-1252, then macintosh.
