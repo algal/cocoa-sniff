c53c92c155e994b7df39a21e3b115979ba71150b
Alexis Gallagher <alexis@alexisgallagher.com>
2012-12-31 12:27:51 +0000

    Initial Commit

---
+++ cocoa-sniff/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  cocoa-sniff
+//
+//  Created by Alexis Gallagher on 2012-12-31.
+//  Copyright (c) 2012 Foxtrot Studios. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+  @autoreleasepool {
+      
+      // insert code here...
+      NSLog(@"Hello, World!");
+      
+  }
+    return 0;
+}
+
